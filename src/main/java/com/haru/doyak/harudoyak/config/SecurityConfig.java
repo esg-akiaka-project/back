b96bd0df17dd69280a5f0d10d5b54da58dba2ce4
KOOHYO <108776113+KOOHYO@users.noreply.github.com>
2024-11-13 12:58:47 +0900

    fix: master브랜치 최근사항으로

--- src/main/java/com/haru/doyak/harudoyak/config/SecurityConfig.java
+++ src/main/java/com/haru/doyak/harudoyak/config/SecurityConfig.java
@@ -4,9 +4,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 
 @Configuration
@@ -19,7 +25,8 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
+
+        /*http
                 .authorizeRequests(authorizeRequests ->
                         authorizeRequests
                                 .anyRequest().permitAll()  // 모든 요청에 대해 인증을 요구하지 않음
@@ -28,8 +35,35 @@ public class SecurityConfig {
                 .headers(headers -> headers.disable())  // X-Frame-Options 비활성화 (필요한 경우)
                 .formLogin(formLogin -> formLogin.disable())  // 기본 로그인 폼 비활성화
                 .httpBasic(httpBasic -> httpBasic.disable())  // HTTP Basic 인증 비활성화
-                .logout(logout -> logout.disable());
+                .logout(logout -> logout.disable());*/
 
-        return http.build();
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                .headers(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .logout(AbstractHttpConfigurer::disable)
+                .build();
     }
+
+    /*
+     * cors관련 설정 커스텀
+     * */
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOriginPatterns(Arrays.asList("*")); // 모든 도메인 및 포트 허용
+        configuration.addAllowedMethod("*"); // 모든 HTTP 메서드 허용
+        configuration.addAllowedHeader("*"); // 모든 헤더 허용
+        configuration.setExposedHeaders(Arrays.asList("Authorization"));
+        configuration.setAllowCredentials(false); // 자격 증명 포함 필요 시 true로 설정
+        configuration.setMaxAge(3600L); // 캐싱 시간 설정 (단위: 초)
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+    }
+
 }
