50645cca6b6e14194d034098f452445ce651ef55
zpfhzpfh112@naver.com <zpfhzpfh112@naver.com>
2024-11-28 15:25:59 +0900

    chore: 주간 도약기록 이상 데이터 확인 후 수정

--- src/main/java/com/haru/doyak/harudoyak/util/DateUtil.java
+++ src/main/java/com/haru/doyak/harudoyak/util/DateUtil.java
@@ -1,14 +1,14 @@
 package com.haru.doyak.harudoyak.util;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Date;
 
 @Component
+@Slf4j
 public class DateUtil {
 
     /*
@@ -19,30 +19,32 @@ public class DateUtil {
     public LocalDateTime stringToLocalDateTime(String dateStr) throws ParseException {
 
         dateStr += " 00:00:00";
+
         // 포맷터
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-        // 문자열 -> Date
-        LocalDateTime date = LocalDateTime.parse(dateStr, formatter);
 
-        return date;
+        // 문자열 -> LocalDateTime 타입으로 변환
+        LocalDateTime resultDate = LocalDateTime.parse(dateStr, formatter);
+
+        return resultDate;
     }
 
-    /*
-     * String 타입 문자열을 LocalDateTime 타입으로 변환하는 메서드
-     * @param : String dateStr
-     * @return : LocalDateTime date
-     * */
-    public Date stringToDate(String dateStr) throws ParseException {
-
-        dateStr += " 00:00:00";
-        // 포맷터
-        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        // 문자열 -> Date
-        Date date = formatter.parse(dateStr);
-        System.out.println(date); // 2021-06-19T21:05:07
-
-        return date;
-    }
+//    /*
+//     * String 타입 문자열을 LocalDateTime 타입으로 변환하는 메서드
+//     * @param : String dateStr
+//     * @return : LocalDateTime date
+//     * */
+//    public Date stringToDate(String dateStr) throws ParseException {
+//
+//        dateStr += " 00:00:00";
+//        // 포맷터
+//        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+//        // 문자열 -> Date
+//        Date date = formatter.parse(dateStr);
+//        System.out.println(date); // 2021-06-19T21:05:07
+//
+//        return date;
+//    }
 
 
 }
